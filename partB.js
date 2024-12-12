5cd3922b166556b79029625769703dc960eaeda3
OrCohen186 <orcohen18695@gmail.com>
2024-12-12 19:36:27 +0200

    partB

--- partB.js
+++ partB.js
@@ -23,3 +23,36 @@ for (let i = 0; i < fruits.length; i++) {
 for (const fruit in fruits) {
   console.log(fruits[fruit]);
 }
+
+//Task 5: Find the Length of an Array
+console.log(fruits.length);
+
+//Task 6: Check for an Element
+function isInArray(array, e) {
+  return array.includes(e);
+}
+console.log(isInArray(fruits, "Kiwi"));
+console.log(isInArray(fruits, "strawberry"));
+
+//Task 7: Sort an Array
+fruits.sort();
+console.log(fruits);
+
+//Task 8: Reverse an Array
+fruits.reverse();
+console.log(fruits);
+
+//Task 9: Filter Array Elements
+const numbers = [2, 55, 45, 86, 98, 31, 7, 50, 22, 26];
+console.log(numbers);
+function filterEvenNumbers(array) {
+  const evenNumbers = array.filter((num) => num % 2 === 0);
+  console.log(evenNumbers);
+  return evenNumbers;
+}
+filterEvenNumbers(numbers);
+
+//Task 10: Combine Two Arrays
+const vegetables = ["carrot", "broccoli", "spinach", "tomato", "cucumber"];
+const groceries = [...fruits, ...vegetables];
+console.log(groceries);
